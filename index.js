70350bca1d1e0d7abc25802cce9c878ef94e1a3a
poornendra parouha <poornendraparouha@gmail.com>
2025-01-11 16:37:56 +0530

    added expressEjsLayouts

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import ProductController from './src/controllers/product.controller.js';
 import path from 'path';
+import expressEjsLayouts from 'express-ejs-layouts';
 
 const server = express();
 
 // setup view engine settings
 server.set('view engine', 'ejs');
 server.set('views', path.join(path.resolve(),"src","views",));
+
+// setup express-ejs-layouts
+server.use(expressEjsLayouts);
 
 // create a new instance of the ProductController 
 const productController = new ProductController();
