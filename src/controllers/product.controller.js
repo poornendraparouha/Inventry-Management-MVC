a12e34038ea568c0ff3494985080dc7a679bf45c
poornendra parouha <poornendraparouha@gmail.com>
2025-01-12 10:10:30 +0530

    added form validation from server side

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,10 +14,27 @@ export default class ProductController{
     }
 
     getAddForm(req, res){
-        return res.render('new-product');
+        return res.render('new-product', {errorMessage: null});
     }
 
     addNewProduct(req, res){
+        // validate the data
+        const {name, price, imageUrl} = req.body;
+        let errors=[];
+        if(!name || name.trim == ''){
+            errors.push({message: 'Name is required'});
+        }
+        if(!price || parseFloat(price) < 1){
+            errors.push({message: 'Price must be greater than 0'});
+        }
+        try{
+            const url = new URL(imageUrl);
+        }catch(err){
+            errors.push({message: 'Invalid URL'});
+        }
+        if(errors.length > 0){
+            return res.render('new-product', {errorMessage: errors[0].message});
+        }
         // access data from the form
         ProductModal.add(req.body);        
         let products = ProductModal.get();
